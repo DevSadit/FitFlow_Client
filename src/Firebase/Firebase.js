8f25e2cccf0535ec2bb18b7e41b2240c6940fb68
DevSadit <dev.sadit@gmail.com>
2025-05-27 11:13:14 +0600

    google login updated

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
  
   apiKey: import.meta.env.VITE_APIKEY,
@@ -13,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APPID,
 };
-
+console.log(import.meta.env.VITE_APIKEY)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
